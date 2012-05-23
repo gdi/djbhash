49d9f8d6836988ba0e65567be95c08e9579c52e6
Jon Durbin <jond@greenviewdata.com>
2012-05-23 14:51:13 -0400

    Updated test file.

--- test.c
+++ test.c
@@ -3,43 +3,43 @@
 int main( int argc, char *argv[] )
 {
   // Hash table structure.
-  struct h_table ht;
+  struct djbhash hash;
   // Return value from finding a key.
-  struct h_node *item;
+  struct djbhash_node *item;
 
-  // Initialize the hash table with 5 linked lists.
-  djbhash_init( &ht, 5, DJBHASH_LINKED_LIST );
+  // Initialize the hash table.
+  djbhash_init( &hash );
 
   // Set hash values with various data types.
   int temp = 10;
-  djbhash_set( &ht, "int", &temp, DJBHASH_INT );
+  djbhash_set( &hash, "int", &temp, DJBHASH_INT );
   double temp2 = 3.14159;
-  djbhash_set( &ht, "double", &temp2, DJBHASH_DOUBLE );
+  djbhash_set( &hash, "double", &temp2, DJBHASH_DOUBLE );
   char temp3 = 'a';
-  djbhash_set( &ht, "char", &temp3, DJBHASH_CHAR );
-  djbhash_set( &ht, "string", "bar", DJBHASH_STRING );
+  djbhash_set( &hash, "char", &temp3, DJBHASH_CHAR );
+  djbhash_set( &hash, "string", "bar", DJBHASH_STRING );
   int temp_arr[] = { 8, 6, 7, 5, 3, 0, 9 };
-  djbhash_set( &ht, "array", temp_arr, DJBHASH_ARRAY, 7 );
+  djbhash_set( &hash, "array", temp_arr, DJBHASH_ARRAY, 7 );
 
   // Find and print items.
-  item = djbhash_find( &ht, "int" );
+  item = djbhash_find( &hash, "int" );
   djbhash_print( item );
-  item = djbhash_find( &ht, "double" );
+  item = djbhash_find( &hash, "double" );
   djbhash_print( item );
-  item = djbhash_find( &ht, "char" );
+  item = djbhash_find( &hash, "char" );
   djbhash_print( item );
-  item = djbhash_find( &ht, "string" );
+  item = djbhash_find( &hash, "string" );
   djbhash_print( item );
-  item = djbhash_find( &ht, "array" );
+  item = djbhash_find( &hash, "array" );
   djbhash_print( item );
 
   // Search for an item that doesn't exist:
   char *missing = "missing key";
-  if ( ( item = djbhash_find( &ht, missing ) ) == NULL )
+  if ( ( item = djbhash_find( &hash, missing ) ) == NULL )
     printf( "%s: No such item!\n", missing );
 
   // Remove all items and free memory.
-  djbhash_destroy( &ht );
+  djbhash_destroy( &hash );
 
   return 0;
 }
