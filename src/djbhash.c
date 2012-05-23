5f5b169db04fb41b4742759a9442631651363d87
Jon Durbin <jond@greenviewdata.com>
2012-05-23 14:50:28 -0400

    Complete re-write...

--- src/djbhash.c
+++ src/djbhash.c
@@ -1,7 +1,7 @@
 #include "djbhash.h"
 
 // Print an items' data.
-void djbhash_print_value( struct h_node *item )
+void djbhash_print_value( struct djbhash_node *item )
 {
   // Loop iterator for array values.
   int i;
@@ -35,7 +35,7 @@ void djbhash_print_value( struct h_node *item )
 }
 
 // Print the key value pair.
-void djbhash_print( struct h_node *item )
+void djbhash_print( struct djbhash_node *item )
 {
   printf( "%s => ", item->key );
   djbhash_print_value( item );
@@ -43,38 +43,14 @@ void djbhash_print( struct h_node *item )
 }
 
 // Initialize the hash table.
-void djbhash_init( struct h_table *ht, int t_size, int t_type )
+void djbhash_init( struct djbhash *hash )
 {
-  // Iterator.
-  int i;
-
-  // Set the table size and allocate some memory.
-  ht->t_size = t_size;
-  ht->ll = malloc( sizeof( struct h_node * ) * t_size );
-
-  // Default the table type.
-  if ( t_type != DJBHASH_ARRAY_LIST && t_type != DJBHASH_LINKED_LIST )
-    t_type = DJBHASH_LINKED_LIST;
-  ht->t_type = t_type;
-
-  if ( t_type == DJBHASH_LINKED_LIST )
-  {
-    // Initialize the lists to be NULL.
-    for ( i = 0; i < ht->t_size; i++ )
-      ht->ll[i] = NULL;
-  } else
-  {
-    ht->count = malloc( sizeof( int ) * t_size );
-    for ( i = 0; i < ht->t_size; i++ )
-    {
-      ht->ll[i] = ( struct h_node * )malloc( sizeof( struct h_node ) * DJBHASH_CHUNK_SIZE );
-      ht->count[i] = 0;
-    }
-  }
+  hash->buckets = malloc( sizeof( struct djbhash_bucket ) * DJBHASH_CHUNK_SIZE );
+  hash->count = 0;
 }
 
 // DJB Hash function.
-unsigned int djb_hash( struct h_table *ht, char *key, int length )
+unsigned int djb_hash( char *key, int length )
 {
   unsigned int i;
   unsigned int hash;
@@ -82,14 +58,16 @@ unsigned int djb_hash( struct h_table *ht, char *key, int length )
   hash = 5381;
   for ( i = 0; i < length; key++, i++ )
     hash = ( ( hash << 5 ) + hash ) + ( *key );
-  return hash % ht->t_size;
+  return hash % 32768;
 }
 
-// Find the position of an element, (or the index where it should be inserted).
-int djbhash_bin_search( struct h_table *ht, char *key, int min, int max, int index, int length, int insert_mode )
+// Find the bucket for the element.
+int djbhash_bin_search( struct djbhash *hash, int min, int max, int bucket_id, char *key, int length, int insert_mode )
 {
+  // Mid-point for search.
   int mid;
-  int cmp;
+  // Linked list iterator.
+  struct djbhash_node *iter;
 
   if ( max < min )
   {
@@ -100,121 +78,42 @@ int djbhash_bin_search( struct h_table *ht, char *key, int min, int max, int ind
   }
 
   mid = ( min + max ) / 2;
-//printf( "min: %d, max: %d, mid: %d, size: %d, index: %d, key: '%s', cmp: '%s'\n", min, max, mid, ht->count[index], index, key, ht->ll[index][mid].key );
-
-  cmp = strncmp( ht->ll[index][mid].key, key, length );
-
-  if ( cmp > 0 )
-    return djbhash_bin_search( ht, key, min, mid - 1, index, length, insert_mode );
-  else if ( cmp < 0 )
-    return djbhash_bin_search( ht, key, mid + 1, max, index, length, insert_mode );
+  if ( hash->buckets[mid].id > bucket_id )
+    return djbhash_bin_search( hash, min, mid - 1, bucket_id, key, length, insert_mode );
+  else if ( hash->buckets[mid].id < bucket_id )
+    return djbhash_bin_search( hash, mid + 1, max, bucket_id, key, length, insert_mode );
   else
-    return mid;
+  {
+    // Point our iterator to the first element in this bucket.
+    iter = hash->buckets[mid].list;
+    while ( iter )
+    {
+      // We want to return if the key in the linked list actually matches.
+      if ( strncmp( iter->key, key, length ) == 0 )
+        return mid;
+      iter = iter->next;
+    }
+
+    // If we got here, there the item doesn't actually exist, it's just a hash collision.
+    if ( insert_mode )
+      return mid;
+    else
+      return -1;
+  }
 }
 
 // Set the value for an item in the hash table using array hash table.
-int djbhash_set_arr( struct h_table *ht, char *key, void *value, int data_type, int index, int length, int count )
+int djbhash_set( struct djbhash *hash, char *key, void *value, int data_type, ... )
 {
   int i;
   int chunks;
   int insert_pos;
-  struct h_node *item;
-  int insert;
-
-  insert_pos = djbhash_bin_search( ht, key, 0, ht->count[index] - 1, index, length, true );
-  insert = true;
-  if ( insert_pos < ht->count[index] )
-  {
-    item = &ht->ll[index][insert_pos];
-    // Check if we're doing an insert or an update.
-    if ( strncmp( item->key, key, length ) == 0 )
-    {
-      insert = false;
-      item->value = value;
-    }
-  }
-  if ( insert )
-  {
-    // See if we need more memory.
-    chunks = ht->count[index] / DJBHASH_CHUNK_SIZE + 1;
-    if ( ht->count[index] > 0 && ht->count[index] % DJBHASH_CHUNK_SIZE == 0 )
-      ht->ll[index] = realloc( ht->ll[index], sizeof( struct h_node ) * ( ( chunks + 1 ) * DJBHASH_CHUNK_SIZE ) );
-
-    // Move all of the bigger elements over.
-    for ( i = ht->count[index]; i > insert_pos; i-- )
-    {
-      // Move the key.
-      ht->ll[index][i].key = ht->ll[index][i - 1].key;
-
-      // Move the value.
-      ht->ll[index][i].value = ht->ll[index][i - 1].value;
-
-      // Move the data type.
-      ht->ll[index][i].data_type = ht->ll[index][i - 1].data_type;
-
-      // Move the array count.
-      ht->ll[index][i].count = ht->ll[index][i - 1].count;
-    }
-    // Add the new element.
-    ht->ll[index][insert_pos].key = strdup( key );
-    ht->ll[index][insert_pos].value = value;
-    ht->ll[index][insert_pos].count = count;
-    ht->ll[index][insert_pos].data_type = data_type;
-    ht->count[index]++;
-  }
-}
-
-// Set the value for an item in the hash table using linked list.
-int djbhash_set_ll( struct h_table *ht, char *key, void *value, int data_type, int index, int length, int count )
-{
-  struct h_node *searcher;
-  struct h_node *temp;
-
-  // Create our hash item.
-  temp = malloc( sizeof( struct h_node ) );
-  temp->key = strdup( key );
-  temp->value = value;
-  temp->data_type = data_type;
-  temp->count = count;
-  temp->next = NULL;
-
-  // Set up the linked list iterator.
-  searcher = ht->ll[index];
-
-  // See if this is the first item or not.
-  if ( searcher == NULL )
-  {
-    ht->ll[index] = temp;
-    return true;
-  }
-
-  // Go to the end of the linked list and check if the key already exists while doing so.
-  while ( searcher->next != NULL )
-  {
-    // Make sure this item doesn't already exist while we're at it.
-    if ( strncmp( searcher->key, key, length ) == 0 )
-    {
-      // Already exists, update the value.
-      searcher->value = value;
-      // Cleanup the temp variable.
-      free( temp );
-      temp = NULL;
-      return true;
-    }
-    searcher = searcher->next;
-  }
-
-  // Set the next item to be the new item.
-  searcher->next = temp;
-  return true;
-}
-
-int djbhash_set( struct h_table *ht, char *key, void *value, int data_type, ... )
-{
-  int count;
-  int index;
+  unsigned int bucket_id;
   int length;
   va_list arg_ptr;
+  struct djbhash_node *iter, *temp;
+  int method;
+  int count;
 
   // Default invalid data types.
   if ( data_type < DJBHASH_INT || data_type > DJBHASH_ARRAY )
@@ -228,23 +127,87 @@ int djbhash_set( struct h_table *ht, char *key, void *value, int data_type, ... 
     va_end( arg_ptr );
   }
 
+  // Calculate the key length and bucket ID.
   length = strlen( key );
-  index = djb_hash( ht, key, length );
+  bucket_id = djb_hash( key, length );
 
-  if ( ht->t_type == DJBHASH_ARRAY_LIST )
-    return djbhash_set_arr( ht, key, value, data_type, index, length, count );
-  else
-    return djbhash_set_ll( ht, key, value, data_type, index, length, count );
+  // Find out which bucket this item belongs to.
+  insert_pos = djbhash_bin_search( hash, 0, hash->count - 1, bucket_id, key, length, true );
+
+  // Create our hash item.
+  temp = malloc( sizeof( struct djbhash_node ) );
+  temp->key = strdup( key );
+  temp->value = value;
+  temp->data_type = data_type;
+  temp->count = count;
+  temp->next = NULL;
+
+  // Find out if we are inserting, updating, or creating this item.
+  method = DJBHASH_INSERT;
+  if ( insert_pos < hash->count )
+  {
+    // Check if we're doing an insert or an update.
+    if ( bucket_id == hash->buckets[insert_pos].id )
+    {
+      method = DJBHASH_APPEND;
+      iter = hash->buckets[insert_pos].list;
+      while ( iter->next != NULL )
+      {
+        if ( strncmp( iter->key, key, length ) == 0 )
+        {
+          method = DJBHASH_UPDATE;
+          break;
+        }
+        iter = iter->next;
+      }
+    }
+  }
+
+  switch ( method )
+  {
+    case DJBHASH_UPDATE:
+      free( temp );
+      temp = NULL;
+      iter->value = value;
+      break; 
+    case DJBHASH_APPEND:
+      iter->next = temp;
+      break;
+    case DJBHASH_INSERT:
+      // See if we need more memory.
+      chunks = hash->count / DJBHASH_CHUNK_SIZE + 1;
+      if ( hash->count > 0 && hash->count % DJBHASH_CHUNK_SIZE == 0 )
+        hash->buckets = realloc( hash->buckets, sizeof( struct djbhash_bucket ) * ( ( chunks + 1 ) * DJBHASH_CHUNK_SIZE ) );
+
+      // Move all of the larger elements over.
+      for ( i = hash->count; i > insert_pos; i-- )
+        hash->buckets[i] = hash->buckets[i - 1];
+
+      hash->buckets[insert_pos].id = bucket_id;
+      hash->buckets[insert_pos].list = temp;
+      hash->count++;
+      break;
+  }
 }
 
 // Find an item in the hash table using linked lists.
-struct h_node *djbhash_find_ll( struct h_table *ht, char *key )
+struct djbhash_node *djbhash_find( struct djbhash *hash, char *key )
 {
   int length;
-  struct h_node *searcher;
+  int bucket_id;
+  int position;
+  struct djbhash_node *searcher;
 
   length = strlen( key );
-  searcher = ht->ll[djb_hash( ht, key, length )];
+  bucket_id = djb_hash( key, length );
+  position = djbhash_bin_search( hash, 0, hash->count - 1, bucket_id, key, length, false );
+
+  // If we don't find the bucket ID, obviously it doesn't exist.
+  if ( position == -1 )
+    return NULL;
+
+  // Otherwise, search through the linked list to find the item.
+  searcher = hash->buckets[position].list;
 
   // Make sure this list isn't empty first.
   if ( searcher == NULL )
@@ -260,100 +223,53 @@ struct h_node *djbhash_find_ll( struct h_table *ht, char *key )
   return NULL;
 }
 
-// Find an item in the hash table using array mode.
-struct h_node *djbhash_find_arr( struct h_table *ht, char *key )
-{
-  int length;
-  int index;
-  int pos;
-
-  length = strlen( key );
-  index = djb_hash( ht, key, length );
-  pos = djbhash_bin_search( ht, key, 0, ht->count[index] - 1, index, length, false );
-
-  if ( pos >= 0 )
-  {
-    return &ht->ll[index][pos];
-  } else
-    return NULL;
-}
-
-// Find an item in the hash.
-struct h_node *djbhash_find( struct h_table *ht, char *key )
-{
-  if ( ht->t_type == DJBHASH_LINKED_LIST )
-    return djbhash_find_ll( ht, key );
-  else
-    return djbhash_find_arr( ht, key );
-}
-
 // Dump all data in the hash table using linked lists.
-void djbhash_dump( struct h_table *ht )
+void djbhash_dump( struct djbhash *hash )
 {
-  int i, j;
-  struct h_node *iter;
+  int i;
+  struct djbhash_node *iter;
 
-  if ( ht->t_type == DJBHASH_LINKED_LIST )
+  for ( i = 0; i < hash->count; i++ )
   {
-    for ( i = 0; i < ht->t_size; i++ )
+    iter = hash->buckets[i].list;
+    while ( iter )
     {
-      iter = ht->ll[i];
-      while ( iter )
-        djbhash_print( iter );
-    }
-  } else
-  {
-    for ( i = 0; i < ht->t_size; i++ )
-    {
-      for ( j = 0; j < ht->count[i]; j++ )
-        djbhash_print( &ht->ll[i][j] );
+      djbhash_print( iter );
+      iter = iter->next;
     }
   }
 }
 
 // Remove all elements from the hash table.
-void djbhash_empty( struct h_table *ht )
+void djbhash_empty( struct djbhash *hash )
 {
-  int i, j;
-  struct h_node *iter;
-  struct h_node *next;
-  for ( i = 0; i < ht->t_size; i++ )
+  int i;
+  struct djbhash_node *iter;
+  struct djbhash_node *next;
+  for ( i = 0; i < hash->count; i++ )
   {
-    if ( ht->t_type == DJBHASH_LINKED_LIST )
+    iter = hash->buckets[i].list;
+    while ( iter )
     {
-      iter = ht->ll[i];
-      while ( iter )
+      if ( iter->key != NULL )
       {
-        if ( iter->key != NULL )
-        {
-          free( iter->key );
-          iter->key = NULL;
-        }
-        next = iter->next;
-        free( iter );
-        iter = next;
+        free( iter->key );
+        iter->key = NULL;
       }
-    } else
-    {
-      for ( j = 0; j < ht->count[i]; j++ )
-      {
-        if ( ht->ll[i][j].key != NULL )
-        {
-          free( ht->ll[i][j].key );
-          ht->ll[i][j].key = NULL;
-        }
-      }
-      ht->count[i] = 0;
-      free( ht->ll[i] );
-      ht->ll[i] = NULL;
+      next = iter->next;
+      free( iter );
+      iter = next;
     }
   }
+  free( hash->buckets );
+  hash->buckets = malloc( sizeof( struct djbhash_bucket ) );
+  hash->count = 0;
 }
 
 // Remove all elements and frees memory used by the hash table.
-void djbhash_destroy( struct h_table *ht )
+void djbhash_destroy( struct djbhash *hash )
 {
-  djbhash_empty( ht );
-  free( ht->ll );
-  ht->ll = NULL;
+  djbhash_empty( hash );
+  free( hash->buckets );
+  hash->buckets = NULL;
 }
--- src/djbhash.h
+++ src/djbhash.h
@@ -10,10 +10,10 @@
   #define false 0
 #endif
 
-#define DJBHASH_CHUNK_SIZE 16
+#define DJBHASH_CHUNK_SIZE 512
 
 // Node structure
-struct h_node {
+struct djbhash_node {
   // Key string.
   char *key;
   // Generic pointer to value.
@@ -23,25 +23,23 @@ struct h_node {
   // If it's an array data type, the number of items.
   int count;
   // Pointer to the next node in the list.
-  struct h_node *next;
+  struct djbhash_node *next;
+};
+
+// Linked list bucket structure.
+struct djbhash_bucket {
+  // Bucket ID.
+  unsigned int id;
+  // Linked list containing items.
+  struct djbhash_node *list;
 };
 
 // Linked list structure.
-struct h_table {
-  // Linked lists composing the hash table.
-  struct h_node **ll;
-  // Number of arrays/linked lists.
-  int t_size;
-  // Type of table to use - Array or Linked list.
-  int t_type;
-  // Number of items in the array when using array mode.
-  int *count;
-};
-
-// Type of hash table (array or linked list)
-enum djbhash_table_type {
-  DJBHASH_ARRAY_LIST,
-  DJBHASH_LINKED_LIST,
+struct djbhash {
+  // Buckets.
+  struct djbhash_bucket *buckets;
+  // Number of buckets.
+  int count;
 };
 
 // Some various return functions.
@@ -53,44 +51,21 @@ enum djbhash_data_type {
   DJBHASH_ARRAY,
 };
 
-// Print just the item's value.
-void djbhash_print_value( struct h_node *item );
+// Method to use when setting a hash item.
+enum djbhash_method {
+  DJBHASH_APPEND,
+  DJBHASH_UPDATE,
+  DJBHASH_INSERT,
+};
 
-// Print an item in format: key => value
-void djbhash_print( struct h_node *item );
 
-// Hash initialization.
-void djbhash_init( struct h_table *ht, int t_size, int t_type );
-
-// DJB string hashing function.
-unsigned int djb_hash( struct h_table *ht, char *key, int length );
-
-// Find an element in the hash using binary search with the array list method.
-int djbhash_bin_search( struct h_table *ht, char *key, int min, int max, int index, int length, int insert_mode );
-
-// Set a hash item using array mode.
-int djbhash_set_arr( struct h_table *ht, char *key, void *value, int data_type, int index, int length, int count );
-
-// Set a hash item using linked list mode.
-int djbhash_set_ll( struct h_table *ht, char *key, void *value, int data_type, int index, int length, int count );
-
-// Generic driver to set a hash item.
-int djbhash_set( struct h_table *ht, char *key, void *value, int data_type, ... );
-
-// Find an element using linked list mode.
-struct h_node *djbhash_find_ll( struct h_table *ht, char *key );
-
-// Find an element using array mode.
-struct h_node *djbhash_find_arr( struct h_table *ht, char *key );
-
-// Generic driver to find a hash item.
-struct h_node *djbhash_find( struct h_table *ht, char *key );
-
-// Print all items in the hash.
-void djbhash_dump( struct h_table *ht );
-
-// Remove all items from the hash.
-void djbhash_empty( struct h_table *ht );
-
-// Remove all items from the hash and free memory.
-void djbhash_destroy( struct h_table *ht );
+void djbhash_print_value( struct djbhash_node *item );
+void djbhash_print( struct djbhash_node *item );
+void djbhash_init( struct djbhash *hash );
+unsigned int djb_hash( char *key, int length );
+int djbhash_bin_search( struct djbhash *hash, int min, int max, int bucket_id, char *key, int length, int insert_mode );
+int djbhash_set( struct djbhash *hash, char *key, void *value, int data_type, ... );
+struct djbhash_node *djbhash_find( struct djbhash *hash, char *key );
+void djbhash_dump( struct djbhash *hash );
+void djbhash_empty( struct djbhash *hash );
+void djbhash_destroy( struct djbhash *hash );
